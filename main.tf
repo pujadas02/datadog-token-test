4b6870751c5c6cf848d2515d33b65f82fa02e279
pujadas02 <pd4759150@gmail.com>
2025-04-14 16:14:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
+# Specify the required Terraform version (optional)
 terraform {
   required_version = ">= 1.0.0"
+
+  # Specify required providers here
+  required_providers {
+    azurerm = {
+      version = "~> 3.0"
+    }
+  }
 }
 
+# Azure Provider configuration
 provider "azurerm" {
   features {}
-
-  version = "~> 3.0"
 }
+
+# A basic resource for testing
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "East US"
